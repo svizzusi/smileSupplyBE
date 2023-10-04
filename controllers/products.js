d9da9ed567872104c6dc6214595e3adea4eec133
Stephen Vizzusi <svizzusi13@gmail.com>
2023-10-03 20:13:45 -0700

    Added functionality to the edit buttons and populated the existing products and the reorder reminder based on frequency

--- controllers/products.js
+++ controllers/products.js
@@ -11,6 +11,7 @@ module.exports = {
             console.log(err) //log the error
         })
     },
+
     createProduct: (req, res) => {
         productSchema.create(req.body) // Add Data to the collection
         .then(Product => {
@@ -21,4 +22,39 @@ module.exports = {
             console.log(err)
         })
     },
+
+    getProduct: (req, res) => {
+        const id = req.params.id // Extract the ID parameter from the request
+        productSchema.findById(id) // Retrieve data by its ID
+        .then(Product => res.json(Product)) // Convert data to JSON and send response
+        .catch(err => {
+            res.json(err)
+            console.log(err)
+        })
+    },
+
+    updateProduct: (req, res) => {
+        const id = req.params.id // Extract the ID parameter from the request
+        productSchema.findByIdAndUpdate(id, { // Find and update data by its ID
+            name: req.body.name,
+            price: req.body.price,
+            quantity: req.body.quantity,
+            frequency: req.body.frequency
+        }) 
+        .then(Product => res.json(Product)) // Convert data to JSON and send response
+        .catch(err => {
+            res.json(err)
+            console.log(err)
+        })
+    },
+
+    deleteProduct: (req, res) => {
+        const id = req.params.id // Extract the ID parameter from the request
+        productSchema.findByIdAndDelete(id) // Find and Delete data by its ID
+        .then(Product => res.json(Product)) // Convert data to JSON and send response
+        .catch(err => {
+            res.json(err)
+            console.log(err)
+        })
+    }
 }
--- routes/products.js
+++ routes/products.js
@@ -6,7 +6,17 @@ const productsController = require('../controllers/products')
 // Route for creating a product
 router.post('/createProduct', productsController.createProduct);
 
+// Express route to the root route for fetching users products by the user id
 router.get('/getProducts/:id', productsController.getProducts)
+
+// Express route to the root route for fetching users products by the product id
+router.get('/getProduct/:id', productsController.getProduct)
+
+// Express route for updating a task by its ID(From MongoDB)
+router.put('/updateProduct/:id', productsController.updateProduct)
+
+// Express route for deleting a product by its ID(From MongoDB)
+router.delete('/deleteProduct/:id', productsController.deleteProduct)
 
 const productRouter = router;
 module.exports = productRouter; // Export the productRouter for use in other parts of the application

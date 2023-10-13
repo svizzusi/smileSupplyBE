22ad53ccd4276c284763906996714a03ad14e5cc
Stephen Vizzusi <svizzusi13@gmail.com>
2023-10-13 10:11:04 -0700

    edited cyclic connections

--- models/Product.js
+++ models/Product.js
@@ -28,7 +28,15 @@ const ProductSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
         required: true,
-    }
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+      },
+      updatedAt: {
+        type: Date,
+        default: Date.now,
+      }
 })
 
 
